764866aea036b8ef3ad9eaf7df1c9174c1d046b0
jrreed83 <joeyrreed@gmail.com>
2024-12-20 15:34:22 -0800

    got google sheets api to work under dev

--- packages/sample/hello/src/hello.ts
+++ packages/sample/hello/src/hello.ts
@@ -1,10 +1,10 @@
-import { digitial_ocean_task } from "./trigger/example";
+import type { digitial_ocean_task } from "./trigger/example";
 import { tasks } from "@trigger.dev/sdk/v3";
 
 export async function main(event: any, context: any) {
     let name = event.name || 'stranger';
-    let greeting = `Hello ${name} `;
-    console.log(greeting)
+    let greeting = `Hello ${name}`;
+    console.log(event);
 
     await tasks.trigger<typeof digitial_ocean_task>(
         "digital-ocean",
--- packages/sample/hello/src/trigger/example.ts
+++ packages/sample/hello/src/trigger/example.ts
@@ -1,16 +1,59 @@
 import { logger, task, wait } from "@trigger.dev/sdk/v3";
+import { google } from "googleapis";
+
+
+// Google Authorization
+
+const jwt_base64  = process.env.GOOGLE_CREDENTIALS_BASE64;
+
+// Thanks to the Trigger.dev documentation for providing this authorization code snippet:
+// https://trigger.dev/docs/deploy-environment-variables#using-google-credential-json-files
+const credentials = JSON.parse(
+  Buffer.from(jwt_base64, "base64").toString("utf8")
+);
+
+const auth = new google.auth.GoogleAuth({
+  credentials,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const client = await auth.getClient();
 
 export const digitial_ocean_task = task({
   id: "digital-ocean",
   // Set an optional maxDuration to prevent tasks from running indefinitely
   maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
+
   run: async (payload: any, { ctx }) => {
     logger.log("Hello, from digital ocean!", { payload, ctx });
 
+    const name = payload.name;
+
     await wait.for({ seconds: 5 });
 
+    const sheets = google.sheets({version: "v4", auth: client});
+    const sheet_id ="1fhOgYe8kxsNeRjU2PFfCSBvaVQ4_rvJzR0acSLLVMxw";
+    ;
+
+    const result = await sheets.spreadsheets.values.get({
+      spreadsheetId: sheet_id,
+      range: "Sheet1!A2:D4"
+    });
+
+    await sheets.spreadsheets.values.update({
+      spreadsheetId: sheet_id,
+      auth: client,
+      range: "Sheet1!E3",
+      resource: { 
+        values: [["hello from trigger.dev"]]
+      },
+      valueInputOption: "USER_ENTERED"
+    });
+    
+    console.log(result.data);
+
     return {
-      message: `Hello, digital ocean from ${payload.name}`
+      message: `Hello, digital ocean from ${name}`
     }
   },
 });
